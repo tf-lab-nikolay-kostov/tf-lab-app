49d27bfc0938039756ae9206df6e6c7bd053b64d
Nikxn91 <nikolay.b.kostov@gmail.com>
2026-08-17 17:00:22 +0300

    fix: configure compatible security linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   extends: ["plugin:security/recommended"],
+
   parserOptions: {
-    "ecmaVersion": 2020
- },
- env: {
-    "es6": true
- }
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
 };
